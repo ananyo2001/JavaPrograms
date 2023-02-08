9f07d26000eab91f1e79e6cb0ef8053b15cb5f59
Ananyo Roy <rgc.ananyo@gmail.com>
2023-02-09 00:14:24 +0530

    Files made!

---
+++ src/com/ananyo/Main.java
@@ -0,0 +1,12 @@
+package com.ananyo;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner input =new Scanner(System.in);
+        System.out.println(input.nextLine());
+        int num1 = input.nextInt();
+        float f1= input.nextFloat();
+
+    }
+}
