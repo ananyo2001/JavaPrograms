7707ba074615cbb958fca249020aee7fc4cd0285
rgcan <rgc.ananyo@gmail.com>
2023-02-09 00:37:36 +0530

    temp converter added

--- src/com/ananyo/Main.java
+++ src/com/ananyo/Main.java
@@ -2,7 +2,7 @@ package com.ananyo;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Hello world!");
         Scanner input =new Scanner(System.in);
         System.out.println(input.nextLine());
         int num1 = input.nextInt();
